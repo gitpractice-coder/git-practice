5090aa1137fade43220891bbc7cdd3de438ce141
jiya sood <jiyasood188@gmail.com>
2026-03-16 00:13:13 +0530

    new file added

---
+++ file3.py
@@ -0,0 +1,11 @@
+def calculate(a,b):
+    c=a+b**2+2+a
+    print(c)
+    return c
+
+print("enter teh value of a: ")
+a=int(input())
+print("enter the value of b: ")
+b=int(input())
+calculate(a,b)
+ 
